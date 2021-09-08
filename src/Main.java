c78c3f959e51e1e75da24f07fd4331f28724414a
Davide Corsi <davide.corsi@par-tec.it>
2021-09-08 17:14:25 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/*
+ * Scrivere un app che legga una sequenza di stringhe e visualizzi:
+ * la stringa lessicograficamente minima
+ * la stringa lessicograficamente massima
+ * la stringa più corta
+ * la stringa più lunga
+ */
+public class Main {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+	}
+}
